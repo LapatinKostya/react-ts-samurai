8610a7eaf88568df1cf6e394f1b4fc65e049df3f
Kostya Lapatin <klrotex11@gmail.com>
2022-05-26 23:20:20 +0300

    12. Lesson React JS (Breakdown into components)

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Navbar = () => {
+    return (
+        <div className={'nav'}>
+            <ul>
+                <a href={'#s'}>
+                    <li>Profile</li>
+                </a>
+                <a href={'#s'}>
+                    <li>Messages</li>
+                </a>
+                <a href={'#s'}>
+                    <li>News</li>
+                </a>
+                <a href={'#s'}>
+                    <li>Music</li>
+                </a>
+                <a href={'#s'}>
+                    <li>Settings</li>
+                </a>
+            </ul>
+        </div>
+    )
+}
