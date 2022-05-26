361079ff0b6bd4e3b18a37bcd3e32288f08e1ba1
Kostya Lapatin <klrotex11@gmail.com>
2022-05-26 21:02:00 +0300

    06. Lesson React JS (index.js, App, JSX)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import {NewComponent} from "./NewComponent";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <ul>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+            <NewComponent/>
+        </div>
+    );
 }
 
 export default App;
